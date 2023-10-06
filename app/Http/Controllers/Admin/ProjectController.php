bbafcf124f10726775408844fba617ae34247877
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-06 20:06:37 +0200

    finish-crud

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -11,17 +11,8 @@ use Illuminate\Support\Str;
 
 class ProjectController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * 
-     * @return View
-     */
-    public function index():View
-    {
-        $projects= Project::all();
 
-        return view("admin.projects.index", compact("projects"));
-    }
+    //CREATE
 
     /**
      * Show the form for creating a new resource.
@@ -32,7 +23,8 @@ class ProjectController extends Controller
         return view("admin.projects.create");
     }
 
-    /**
+
+        /**
      * Store a newly created resource in storage.
      * @param Request $request
      * @return RedirectResponse
@@ -46,7 +38,7 @@ class ProjectController extends Controller
             "description"=>"required|string",
             "thumb"=>"required|string",
             "release"=>"required|date",
-        ]);
+        ]); 
 
         //creo l'istanza di Project, fill per assegnare i dati all'istanza
         //save per salvarli nel database
@@ -55,6 +47,22 @@ class ProjectController extends Controller
 
         return redirect()->route("admin.projects.show", $project->id);
     }
+
+    //READ
+
+    
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return View
+     */
+    public function index():View
+    {
+        $projects= Project::all();
+
+        return view("admin.projects.index", compact("projects"));
+    }
+ 
 
     /**
      * Display the specified resource.
@@ -68,27 +76,59 @@ class ProjectController extends Controller
         return view("admin.projects.show", compact("project"));
     }
 
+
+    //UPDATE
+
+    
     /**
      * Show the form for editing the specified resource.
+     * @param int $id
+     * @return View
      */
-    public function edit(string $id)
+    public function edit(int $id):View
     {
-        //
+        $project = Project::findOrFail($id);
+
+        return view("admin.projects.edit", compact("project"));
     }
 
     /**
      * Update the specified resource in storage.
+     * @param int $id
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id):RedirectResponse
     {
-        //
+        $project = Project::findOrFail($id);
+
+        $data = $request->validate([
+            "title"=>"required|string",
+            "language"=>"required|string",
+            "link"=>"required|string",
+            "description"=>"required|string",
+            "thumb"=>"required|string",
+            "release"=>"required|date",
+        ]); 
+
+        $project->update($data);
+
+        return redirect()->route("admin.projects.show", $project->id);
+
     }
+
+    //DESTROY 
 
     /**
      * Remove the specified resource from storage.
+     * @param int $id
+     * @return RedirectResponse
      */
-    public function destroy(string $id)
+    public function destroy(int $id):RedirectResponse
     {
-        //
+        $project = Project::findOrFail($id);
+        $project->delete();
+
+        return redirect()->route("admin.projects.index");
     }
 }
